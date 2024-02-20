1426f0a06aee0d2fdfc83fd161dc9dba7a86e9ed
TechSiren <asinha27072002@gmailo.com>
2024-02-20 12:01:11 +0530

    completing the navbar

--- netflix-ui/src/components/Navbar.jsx
+++ netflix-ui/src/components/Navbar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
-function Navbar() {
-  return <div></div>;
+export default function Navbar({ isScrolled }) {
+  const links = [
+    { name: "Home", link: "/" },
+    { name: "TV shows", link: "/tv" },
+    { name: "Movies", link: "/movies" },
+    { name: "My List", link: "/mylist" },
+  ];
+
+  return (
+    <Container>
+      <nav className={`flex ${isScrolled ? "scrolled" : ""}`}>
+        <div className="left flex a-center">
+          <div className="brand flex a-center j-center">
+            <img src={logo} alt="logo" />
+          </div>
+          <ul className="links flex">
+            {links.map((name, link) => {
+              return (
+                <li key={name}>
+                  <Link to={link}>{name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </Container>
+  );
 }
-
-export default Navbar;
+const Container = styled.div``;
