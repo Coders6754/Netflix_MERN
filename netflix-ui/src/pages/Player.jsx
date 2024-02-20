ed444bf614d5fa9719baca3142413e54824bb16b
TechSiren <asinha27072002@gmailo.com>
2024-02-20 13:19:10 +0530

    designin gthe player

--- netflix-ui/src/pages/Player.jsx
+++ netflix-ui/src/pages/Player.jsx
@@ -18,4 +18,17 @@ export default function Player() {
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+.player {
+  width: 100vw;
+  height: 100vh;
+  .back {
+    position: absolute;
+    padding: 2rem;
+    z-index: 1;
+    svg {
+      font-size: 3rem;
+      cursor: pointer;
+    }
+  }
+`;
