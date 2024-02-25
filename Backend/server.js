7a35858375c6d97e1be4444a9ebd1f940cd9adc0
MridulKatara <kataramridul2@gmail.com>
2024-02-25 21:43:07 +0530

    test

--- Backend/server.js
+++ Backend/server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI,
-    { useNewUrlParser: true,
-      useUnifiedTopology: true }
+    // { useNewUrlParser: true,
+    //   useUnifiedTopology: true }
   )
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
